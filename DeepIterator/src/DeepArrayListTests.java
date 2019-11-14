260fe908a984add17e0f518016e6933db4818461
Timothy Baba <timothyybaba@gmail.com>
2019-11-14 17:22:15 -0600

    added tests for regrow a rray

--- DeepIterator/src/DeepArrayListTests.java
+++ DeepIterator/src/DeepArrayListTests.java
@@ -108,7 +108,7 @@ public class DeepArrayListTests {
         assertArrayEquals(expected, stringList.getBackingArray());
     }
     @Test(timeout = TIMEOUT)
-    public void testDouble() {
+    public void testAddAtIndexDouble() {
         assertEquals(0, stringList.size());
         stringList.add("0a", 0);
         stringList.add("1a", 0);
@@ -135,6 +135,37 @@ public class DeepArrayListTests {
         expected[8] = "2a";
         expected[9] = "1a";
         expected[10] = "0a";
+        assertArrayEquals(expected, stringList.getBackingArray());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testAddDouble() {
+        assertEquals(0, stringList.size());
+        stringList.add("0a");
+        stringList.add("1a");
+        stringList.add("2a");
+        stringList.add("3a");
+        stringList.add("4a");
+        stringList.add("5a");
+        stringList.add("6a");
+        stringList.add("7a");
+        stringList.add("8a");
+        stringList.add("9a");
+        stringList.add("10a");
+        assertEquals(11, stringList.size());
+
+        Object[] expected = new Object[20]; //this is what the size of the array should be when resizing if doubled
+        expected[0] = "0a";
+        expected[1] = "1a";
+        expected[2] = "2a";
+        expected[3] = "3a";
+        expected[4] = "4a";
+        expected[5] = "5a";
+        expected[6] = "6a";
+        expected[7] = "7a";
+        expected[8] = "8a";
+        expected[9] = "9a";
+        expected[10] = "10a";
         assertArrayEquals(expected, stringList.getBackingArray());
     }
 
